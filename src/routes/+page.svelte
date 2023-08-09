365efa7b42ee0d8541eff3688b5600d940f8e538
dev-viinz <42003446+dev-viinz@users.noreply.github.com>
2023-08-09 16:30:58 +0200

    update front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
-<script>
-
+<script lang="ts">
+    export let data;
 </script>
 
 <div class="flex flex-col gap-y-4 items-center justify-center h-2/3">
     <div class="font-semibold text-6xl md:text-7xl lg:text-9xl text-center gradient-heading">
         Clock IT!
     </div>
-    <a class="btn variant-outline-secondary" href="/login">Login</a>
+    {#if !data.session}
+        <a class="btn variant-outline-secondary" href="/login">Login</a>
+    {:else}
+        <a class="btn variant-outline-secondary" href="/app">To the App</a>
+    {/if}
 </div>
